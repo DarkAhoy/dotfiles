b561edc7703f93cdfb16eb99ee3d6b7b2f9786c3
amos <amosariv91@gmail.com>
2022-01-17 00:17:24 +0200

    basic polybar support for i3

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+kill -9 $( pgrep polybar )
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar top 2>~/.config/polybar/poly.log &
+
+echo "Polybar launched..."
