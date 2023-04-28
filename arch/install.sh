dc7a495f2a2f8ff1fa4e6d026e1590af76680ad3
Amos <amos@overcasrsecurity.com>
2023-04-28 23:56:33 +0300

    Added titus install

---
+++ arch/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#### Check for yay ####
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "$COK - yay was located, moving on."
+    yay -Suy
+else 
+    echo -e "$CWR - Yay was NOT located"
+    read -n1 -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (y,n) ' INSTYAY
+    if [[ $INSTYAY == "Y" || $INSTYAY == "y" ]]; then
+        git clone https://aur.archlinux.org/yay-git.git &>> $INSTLOG
+        cd yay-git
+        makepkg -si --noconfirm &>> ../$INSTLOG
+        cd ..
+        
+    else
+        echo -e "$CER - Yay is required for this script, now exiting"
+        exit
+    fi
+fi
+
+### Install all of the above pacakges ####
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    yay -R --noconfirm swaylock waybar
+    yay -S --noconfirm hyprland polkit-gnome ffmpeg neovim viewnior \
+    rofi pavucontrol thunar starship wl-clipboard wf-recorder     \
+    swaybg grimblast-git ffmpegthumbnailer tumbler playerctl      \
+    noise-suppression-for-voice thunar-archive-plugin kitty       \
+    waybar-hyprland wlogout swaylock-effects sddm-git pamixer     \
+    nwg-look-bin nordic-theme papirus-icon-theme dunst otf-sora   \
+    ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font    \
+    ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa  \
+    ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd  \
+    adobe-source-code-pro-fonts
+fi
